49c433bfa62b1bfef02f6bd3b75e2bfdb8ee6a3f
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-08-12 16:29:37 -0300

    automation

--- test_selenium.py
+++ test_selenium.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 browser.get("https://ri.magazineluiza.com.br/")
-browser.find_element_by_xpath('//*[@id="owl-destaques"]/div[1]/div/div[4]/div/a/p').click()
-browser.find_element_by_xpath('//*[@id="Form1"]/div[15]/div[3]/div/div/div[1]/ul/li[2]/a').click()
+browser.find_element_by_xpath('//*[@id="Form1"]/div[8]/div/div[2]/div/input').send_keys('Magazine Luiza')
+browser.find_element_by_xpath('//*[@id="Form1"]/div[8]/div/div[2]/div/span').click()
