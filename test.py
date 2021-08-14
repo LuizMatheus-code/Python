1afdeab39b005e30b4caeccb15e547f440e36f74
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-08-14 17:08:37 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+#receive the values
+salary = float(input("type here the salary: "))
+expenses = float(input("type here the expenses: "))
+
+#it calculates the difference
+difference = salary - expenses
+
+#it shows the answer
+print("there is {}".format(difference))
