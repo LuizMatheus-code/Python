e1559f21b325c09bd9f477f424a10c0513a3ca33
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-08-16 18:52:54 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
-from math import sqrt
+from math import pi
+import PySimpleGUI as sg
 
-total = []
-notas = []
-dado1 = 1
-while True:
-    dado1 = int(input("digite algo: "))
-    if dado1 == 0:
-        break
-    else:
-        dado2 = int(input("digite algo: "))
-        total.append(dado1)
-        total.append(dado2)
+class PythonScreen:
+    def __init__(self):
+        layout = [
+            [sg.Text('Pi = 3.14')],
+            [sg.Text('Radius', size=(5, 0)), sg.Input(size=(22, 0), key=('Radius'))],
+            [sg.Button('Result', key=('Result')), sg.Button('Cancel', key=('Cancel'))],
+            [sg.Output()]
+        ]
+        self.window = sg.Window("Circle radius").layout(layout)
+    def Start(self):
+        while True:
+            self.button, self.values = self.window.Read()
+            if self.button == "Cancel":
+                break
+            radius = float(self.values['Radius'])
+            area = pi * (radius**2)
+            print('''Radius: {}\nArea: {:.2f}'''.format(radius, area))
 
-contador = 0
-for item in total:
-    notas.append(total[contador])
-    contador += 1
-    if len(notas) == 2:
-        media = (notas[0] + notas[1])/2
-        print("media {}: {}".format(int(contador/2), media))
-        notas = []
+open_screen = PythonScreen()
+open_screen.Start()
