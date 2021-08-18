fb589edb0f9c63c1a47a007ec36a517a03a8ac4a
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-08-18 16:40:58 -0300

    Update test.py

--- test.py
+++ test.py
@@ -13,4 +13,31 @@ def Fibo(limit):
             before_last = last
             last = next_number
 
-Fibo(100)
+
+def Sequence(number):
+    number = float(number)
+    for first in range(1, 11):
+        result = number * first
+        print("{} x {} = {}".format(number, first, result))
+
+
+def Hypotenuse(oposite_cathetus, adjacent_cathetus):
+    oposite_cathetus = float(oposite_cathetus)
+    adjacent_cathetus = float(adjacent_cathetus)
+    hipo = (oposite_cathetus**2 + adjacent_cathetus**2)**(1/2)
+    return hipo
+
+
+import random
+
+options = []
+for num in range(1, 5):
+    x = input('person {}: '.format(num))
+    options.append(x)
+
+choosen_one = random.choice(options)
+print("choosen one: {} out of ".format(choosen_one), end="")
+
+options.remove(choosen_one)
+for elements in options:
+    print(elements, end=" ")
