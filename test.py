fb52203f3a964abc3ff85c0d53c505d2ea72396f
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-08-27 17:32:58 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-number = int(input("insert number: "))
-unity = number // 1 % 10
-ten = number // 10 % 10
-hundred = number // 100 % 10
-thousand = number // 1000 % 10
-print("unity: {}\nten: {}\nhundred: {}\nthousand: {}".format(
-    unity,
-    ten,
-    hundred,
-    thousand
-))
+name = input("type here any name: ").strip()
+
+# prints each letter of the first name until it's fully printed
+print("first name: ", end='')
+for first_lyrics in range(0, name.find(' ')):
+    print(name[first_lyrics], end="")
+
+#separates the two names and, then writes the last name
+print("\nlast name: ", end="")
+for last_lyrics in range(name.rfind(' ') + 1, len(name)):
+    print(name[last_lyrics], end="")
