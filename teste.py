be95584f9e8fb71d6bc60e89e69179ec9dcf36a0
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-09-04 11:43:29 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,20 +1,18 @@
-money = int(input('how much money do you want in dollars? '))
-total = money
-banknote = 50
-total_banknote = 0
+storage = list()
+counter = 0
 while True:
-    if total >= banknote:
-        total -= banknote
-        total_banknote += 1
+    adding = int(input("type a number: "))
+    if adding not in storage:
+        storage.append(adding)
+        counter += 1
     else:
-        if total_banknote > 0:
-            print(f'{total_banknote} of ${banknote}')
-        if banknote == 50:
-            banknote = 20
-        elif banknote == 20:
-            banknote = 10
-        elif banknote == 10:
-            banknote = 1
-        total_banknote = 0
-        if total == 0:
-            break
+        print("duplicated values cannot be added")
+    print('[0] continue\n[1] stop')
+    continue_question = int(input('type: '))
+    if continue_question == 1:
+        break
+print(f'total of elements {counter}')
+print(storage)
+storage.sort(reverse=True)
+print(storage)
+print(storage.index(5))
