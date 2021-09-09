7da53dca80416d63372ff41c537cb2d8be238798
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-09-09 17:51:50 -0300

    Update function_practice.py

--- function_practice.py
+++ function_practice.py
@@ -3,4 +3,22 @@ def area(base, height):
     return area
 
 
+def decoration(word):
+    word = str(word)
+    length_word = len(word) + 2
+    print('=' * length_word)
+    print(word.center(length_word))
+    print('=' * length_word)
+
+
+def counting():
+    start = int(input('start: '))
+    end = int(input('end: '))
+    steps = int(input('steps:'))
+    for showing in range(start, end + 1, steps):
+        print(showing, end=' ')
+
+
 print(area(base=2, height=5))
+decoration('coding')
+counting()
