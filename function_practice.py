5d407d8ad676a34fcd3acd55351d9ef1f24d408a
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-09-10 17:06:49 -0300

    Update function_practice.py

--- function_practice.py
+++ function_practice.py
@@ -19,6 +19,32 @@ def counting():
         print(showing, end=' ')
 
 
+def biggest(*numbers):
+    amount_numbers = []
+    for elements in numbers:
+        amount_numbers.append(elements)
+    bigger = amount_numbers[0]
+    for checking in amount_numbers:
+        if bigger < checking:
+            bigger = checking
+    return bigger
+
+
+def factorial(*num):
+    total_numbers = []
+    final_result = []
+    for elements in num:
+        total_numbers.append(elements)
+    for number_used in total_numbers:
+        final_factorial = 1
+        for count in range(number_used, 0, -1):
+            final_factorial *= count
+        final_result.append(final_factorial)
+    return final_result
+
+
 print(area(base=2, height=5))
 decoration('coding')
-counting()
+print(biggest(10, 2,3 , 5, 6, 11))
+x = factorial(5, 3, 2, 4, 6)
+print(x)
