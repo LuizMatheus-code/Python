d18619f95e918d5eb76556a5e87a13b1ae6c6e60
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-09-13 11:13:04 -0300

    functions

--- Functions/operations.py
+++ Functions/operations.py
@@ -62,3 +62,21 @@ def MoneyConvert(number_list):
         converted_numbers.append(converted)
     return converted_numbers
 
+
+def Summary(value, raising=0, loweing=0):
+    espace = '=' * 20 + ('=' * len(str(Double(value))))
+    print(espace)
+    print('Summary'.center(len(espace)))
+    print(espace)
+    original_value = MoneyConvert([value])[0]
+    print(f'Original: \t{original_value}')
+    double_value = Double(value, convert=True)
+    print(f'Double: \t{double_value[0]}')
+    half_of_value = Half(value, convert=True)
+    print(f'Half: \t\t{half_of_value[0]}')
+    raising_value = Raise(value, percentage=raising, convert=True)
+    print(f'{raising}% Raised: \t{raising_value[0]}')
+    lowering_value = Lower(value, percentage=loweing, convert=True)
+    print(f'{loweing}% Lowered: \t{lowering_value[0]}')
+    print('-' * 20 + ('-' * len(str(Double(value)))))
+
--- module and import/importing.py
+++
@@ -1,4 +0,0 @@
-from money import Half
-
-x = Half(1, 2, 3, convert=True)
-print(x)
