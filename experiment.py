92d7ba6cc2731bbbcc76220b83b72c2354f705d7
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-09-30 16:17:26 -0300

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,13 +1,41 @@
-class Data:
-    def __init__(self, dia, mes, ano):
-        self.dia = dia
-        self.mes = mes
-        self.ano = ano
-
+class Carro:
+    def __init__(self, velocidade):
+        self.velocidade = velocidade
+    
 
     def __str__(self):
-        return f'{self.dia}/{self.mes}/{self.ano}'
+        return self.velocidade
 
 
-d1 = Data(1, 12, 2019)
-print(d1)
+    def acelerar(self, quantidade):
+        if self.velocidade >= 180:
+            return None
+        else:
+            if quantidade + self.velocidade > 180:
+                excedente = 180 - (quantidade + self.velocidade) 
+                ideal = quantidade + excedente
+                self.velocidade += ideal
+                return self.velocidade
+            else:
+                self.velocidade += quantidade
+                return self.velocidade
+
+
+    def frear(self, quantidade):
+        if self.velocidade <= 0:
+            return None
+        else:
+            if self.velocidade - quantidade < 0:
+                    self.velocidade = 0
+                    return self.velocidade
+            else:
+                self.velocidade -= quantidade
+                return self.velocidade
+
+
+fusca = Carro(1)
+print(fusca.velocidade)
+fusca.acelerar(178)
+print(fusca.velocidade)
+fusca.frear(1000)
+print(fusca.velocidade)
