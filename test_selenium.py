eb29320d4327dac301b9d6145978c3ede187c2a2
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-10-06 18:37:48 -0300

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
-browser.get("https://ri.magazineluiza.com.br/")
-browser.find_element_by_xpath('//*[@id="Form1"]/div[8]/div/div[2]/div/input').send_keys('Magazine Luiza')
-browser.find_element_by_xpath('//*[@id="Form1"]/div[8]/div/div[2]/div/span').click()
+browser.get("https://www.google.com")
+print(browser.title)
+browser.quit()
