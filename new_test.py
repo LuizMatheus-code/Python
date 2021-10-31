fdd59831da67c747a16a18b69b8ad3cae6dfa598
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-10-31 17:21:31 -0300

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -1,6 +1,7 @@
-def tag_block(content, clas='success', inline=False):
+def tag_block(content, *args, clas='success', inline=False):
     tag = 'span' if inline else 'div'
-    return f'<{tag} class"{clas}">{content}</{tag}>'
+    html = content if not callable(content) else content(*args)
+    return f'<{tag} class"{clas}">{html}</{tag}>'
 
 
 def tag_list(*itens):
@@ -9,9 +10,21 @@ def tag_list(*itens):
 
 
 if __name__ == '__main__':
-    print(tag_block('block'))
-    print(tag_block('inline and class', 'info', True))
+    '''print(tag_block('block'))
+    print(tag_block('inline and class', 'info', inline=True))
     print(tag_block('inline', inline=True))
     print(tag_block(inline=True, content='inline'))
     print(tag_block('failure', clas='error'))
     print(tag_block(tag_list('item 1', 'item 2'), clas='info'))
+    print(tag_block(tag_list, 'Sunday', 'Saturday', clas='info', inline=True))'''
+
+
+    def result_f1(first, second, third):
+        print(f'1 {first}')
+        print(f'2 {second}')
+        print(f'3 {third}')
+
+
+    podium = {'first' : 'j', 'second' : 'k', 'third' : 'r'}
+    result_f1(**podium)
+
