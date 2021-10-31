cf2d10ba5e00b5eaf839ba71c2cad3da13cdb177
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-10-31 14:06:39 -0300

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -1,11 +1,17 @@
-def tag_block(text, clas='success', inline=False):
+def tag_block(content, clas='success', inline=False):
     tag = 'span' if inline else 'div'
-    return f'<{tag} class"{clas}">{text}</{tag}>'
+    return f'<{tag} class"{clas}">{content}</{tag}>'
+
+
+def tag_list(*itens):
+    lis = ''.join((f'<li>{item}</li>' for item in itens))
+    return f'<ul>{lis}</ul>'
 
 
 if __name__ == '__main__':
     print(tag_block('block'))
     print(tag_block('inline and class', 'info', True))
     print(tag_block('inline', inline=True))
-    print(tag_block(inline=True, text='inline'))
+    print(tag_block(inline=True, content='inline'))
     print(tag_block('failure', clas='error'))
+    print(tag_block(tag_list('item 1', 'item 2'), clas='info'))
