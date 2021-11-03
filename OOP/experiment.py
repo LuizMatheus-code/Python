a7034d03591f24a16ad4e067c574f5126a48d2b9
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-11-03 19:21:49 -0300

    adventure time

--- OOP/experiment.py
+++ OOP/experiment.py
@@ -1,58 +1,28 @@
 from datetime import datetime
-from random import randint
 
-class Person():
-    current_year = int(datetime.strftime(datetime.now(), '%Y'))
-
-    def __init__(self, name, age, speaking=False, eating=False):
+class Person:
+    def __init__(self, name, age, fighting=False, sleeping=False):
         self.name = name
         self.age = age
-        self.speaking = speaking
-        self.eating = eating
+        self.fighting = fighting
+        self.sleeping = sleeping
 
 
-    def eat(self):
-        if self.eating:
-            print(f'{self.name} is already eating')
-            return
-        elif self.speaking:
-            print(f'{self.name} cannot eat while speaking')
-            return
+    def fight(self, enemy):
+        if self.sleeping:
+            print(f"{self.name} can't fight while sleeping")
         else:
-            print(f'{self.name} started eating')
-            self.eating = True
-            return self.eating
+            self.fighting = True
+            print(f'{self.name} is fighting against {enemy}')
+            return 
+
+    def sleep(self):
+        self.sleeping = True
+        print(f'{self.name} is sleeping. Z...Z...Z')
 
 
-    def stop_eating(self):
-        if not self.eating:
-            print(f'{self.name} is not eating')
-        else:
-            self.eating = False
-            print(f'{self.name} stopped eating')
-            return self.eating
-    
+    def year_of_being_born(self):
+        current_year = datetime.now().year
+        final_year = current_year - self.age
+        print(f'{self.name} was born in {final_year}')
 
-    def speak(self, subject):
-        if self.eating:
-            print(f'{self.name} needs to stop eating first')
-        elif self.speaking:
-            print(f'{self.name} is already speaking')
-        else:
-            print(f'{self.name} is talking about {subject}')
-            self.speaking = True
-            return self.speaking
-
-
-    def get_year(self):
-        return self.current_year - self.age
-
-    @classmethod
-    def use_year(cls, name, year_born):
-        age = cls.current_year - year_born
-        return cls(name, age)
-    
-    @staticmethod
-    def ID_generator():
-        rand = randint(10000, 19999)
-        return rand
--- OOP/test.py
+++ OOP/test.py
@@ -1,16 +1,13 @@
 from experiment import Person
 
-p1 = Person("name1", 1)
-p2 = Person('name2', 2)
+finn = Person('Finn, the human', 17)
+fern = Person('Fern, the Grass', 17)
 
-p1.stop_eating()
-p1.eat()
-p1.eat()
-p1.speak('subject')
-p1.stop_eating()
-p1.speak('subject')
-p1.speak('subject')
-x = p1.get_year()
-print(x)
-y = p1.use_year('stuff', 2000)
-print(y.age)
+fern.fight('the emissary from beyond')
+finn.fight('the lich')
+
+finn.sleep()
+fern.sleep()
+
+fern.year_of_being_born()
+finn.year_of_being_born()
