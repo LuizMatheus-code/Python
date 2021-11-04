b7aa89c35103a020f4d1ddd08c6381687a3effb2
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-11-04 05:45:56 -0300

    ok

--- OOP/experiment.py
+++ OOP/experiment.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 class Person:
+    current_year = datetime.now().year
+    weapon = "sword"
+
     def __init__(self, name, age, fighting=False, sleeping=False):
         self.name = name
         self.age = age
@@ -16,13 +19,59 @@ class Person:
             print(f'{self.name} is fighting against {enemy}')
             return 
 
+    @classmethod
+    def weaponry(cls, name):
+        print(f'{name} uses a {cls.weapon} as a weapon')
+
+
+    @staticmethod
+    def call_jake(name, jake_name='Jake, the dog'):
+        print(f'{name} is calling {jake_name}')
+
     def sleep(self):
         self.sleeping = True
         print(f'{self.name} is sleeping. Z...Z...Z')
 
 
     def year_of_being_born(self):
-        current_year = datetime.now().year
-        final_year = current_year - self.age
+        final_year = self.current_year - self.age
         print(f'{self.name} was born in {final_year}')
 
+
+    @classmethod
+    def by_year_of_being_born(cls, name, year):
+        return cls(name, year)
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    
+    def reduce_price(self, percentage=0):
+        self.price -= (self.price * (percentage/100))
+        return self.price
+
+
+    @property
+    def name(self):
+        return self._name
+
+
+    @name.setter
+    def name(self, value):
+        self._name = value.title()
+
+
+    #getter
+    @property
+    def price(self):
+        return self._price
+    
+
+    #setter
+    @price.setter
+    def price(self, value):
+        if isinstance(value, str):
+            value = float(value.replace("R$", ''))
+        self._price = value
--- OOP/test.py
+++ OOP/test.py
@@ -1,13 +1,12 @@
+from experiment import Product
 from experiment import Person
 
-finn = Person('Finn, the human', 17)
-fern = Person('Fern, the Grass', 17)
-
-fern.fight('the emissary from beyond')
-finn.fight('the lich')
-
-finn.sleep()
-fern.sleep()
-
-fern.year_of_being_born()
-finn.year_of_being_born()
+if __name__ == "__main__":
+    finn = Person('Finn, the human', 17)
+    fern = Person('Fern, the Grass', 17)
+    
+    book = Product('Enchiridion', "100.60")
+    
+    print(book.price)
+    print(type(book.price))
+    print(book.name)
