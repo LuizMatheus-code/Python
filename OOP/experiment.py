d86e85a781c3dccd8d6c40c0ede51d35766023c8
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-11-12 07:33:03 -0300

    Update experiment.py

--- OOP/experiment.py
+++ OOP/experiment.py
@@ -1,11 +1,31 @@
 #usr/local/bin/python3
-class Human:
+from abc import ABCMeta, abstractproperty
+
+class Human(metaclass=ABCMeta):
     # class attribute below
     species = 'Homo Sapiens'
 
     def __init__(self, name):
         self.name = name
         self._age = None
+
+
+    @abstractproperty
+    def inteligence(self):
+        pass
+
+
+    
+    @property
+    def age(self):
+        return self._age
+
+    
+    @age.setter
+    def age(self, age):
+        if age < 0:
+            raise ValueError('the age has to be a positive number')
+        self._age = age
 
 
     def ancient(self):
@@ -27,15 +47,27 @@ class Neanderthal(Human):
     species = Human.species_example()[-2]
 
 
+    @property
+    def inteligence(self):
+        return False
+
+
 class HomoSapiens(Human):
     species = Human.species_example()[-1]
 
 
+    @property
+    def inteligence(self):
+        return True
+
+
 if __name__ == '__main__':
-    jose = HomoSapiens('Jose')
-    gork = Neanderthal('gork')
-    print(f'(class) Evolution {", ".join(HomoSapiens.species_example())}')
-    print()
-    print(f'(instance) Evolution {", ".join(jose.species_example())}')
-    print(f'is it evolution? {HomoSapiens.is_evolution()}')
-    print(f'is it evolution? {Neanderthal.is_evolution()}')
+    """try:
+        jose = Human('Jhon')
+        print(jose.inteligence)
+    except TypeError as error:
+        print(error)"""
+
+    k = Neanderthal('kable')
+    print('{} of the class {}, inteligence {}'
+        .format(k.name, k.__class__.__name__, k.inteligence))
