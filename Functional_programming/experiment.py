4b75bf0200caf1e318546321492f341a72eef018
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-11-18 19:01:46 -0300

    Update experiment.py

--- Functional_programming/experiment.py
+++ Functional_programming/experiment.py
@@ -1,6 +1,24 @@
-def mapping(func, lis):
-    return (func(element) for element in lis)
+def _mdc_(x, y):
+    while (y):
+        x = y
+        y = x % y
+    return x
 
 
-xy = mapping(lambda x: x ** 2, [2, 3, 4])
-print(list(xy))
+def mdc(numbers):
+    if len(numbers) == 2:
+        return _mdc_(numbers[0], numbers[1])
+    else:
+        mdc_value = _mdc_(numbers[0], numbers[1])
+        numbers[0] = mdc_value
+        del numbers[1]
+        return mdc(numbers)
+
+
+if __name__ == '__main__':
+    print(mdc([21, 7])) #7
+    print(mdc([125, 40])) #5
+    print(mdc([9, 564, 66, 3])) #3
+    print(mdc([55, 22])) #11
+    print(mdc([15, 150])) #15
+    print(mdc([7, 9])) #1
