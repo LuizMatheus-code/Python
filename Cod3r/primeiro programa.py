d1d5e7bf2934acbfadf8e8dcc84a876dcdb1547c
Luiz Matheus <nomenenhum190@gmail.com>
2023-06-21 07:11:27 -0300

    ok

---
+++ Cod3r/primeiro programa.py
@@ -0,0 +1,3 @@
+print("primeiro programa")
+
+help(print)
---
+++ Cod3r/tipos básicos.py
@@ -0,0 +1,19 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print("Voce" + 3 * "muito" + "legal")
+print([1, 2, 3])
+print({'nome': 'pedro', 'idade': 22})
+
+a = 10
+b = 5.2
+
+print(a + b)
+
+#comentario
+
+'''Comentario
+bem
+longo'''
+
+teste = True
